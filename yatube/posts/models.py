60388e370ec542d52be0159b2a6a01ac4dd2f3f1
Вячеслав Мачулин <machulin.vya4eslav@ya.ru>
2022-09-30 00:49:35 +0300

    Спринт 6, в3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -95,12 +95,3 @@ class Follow(models.Model):
         related_name='following',
         verbose_name='Автор'
     )
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['user', 'author'],
-                name='unique_follow'
-            )
-
-        ]
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 handler404 = 'core.views.page_not_found'
+
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
