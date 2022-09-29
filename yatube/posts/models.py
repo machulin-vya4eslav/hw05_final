8548bf6766ec79e5eeb6f90c0860def833663d46
Вячеслав Мачулин <machulin.vya4eslav@ya.ru>
2022-09-30 00:57:30 +0300

    Спринт 6, в3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -95,3 +95,12 @@ class Follow(models.Model):
         related_name='following',
         verbose_name='Автор'
     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_follow'
+            )
+
+        ]
